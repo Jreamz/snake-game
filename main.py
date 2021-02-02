04ef82bacd259ba9f74085430c20dafbf8f6bfd2
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-02-02 04:15:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from score import Score
+
 import time
 
 screen = Screen()
@@ -8,7 +11,9 @@ screen.bgcolor("black")
 screen.title("SN4K3")
 screen.tracer(0)
 
+score = Score()
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,10 +26,25 @@ game_over = False
 while not game_over:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    #detect collision with food
+    if snake.head.distance(food) < 17:
+        food.refresh()
+        snake.extend()
+        score.add_score()
 
+    #detect collision with boundary
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_over = True
+        score.end_game()
+
+    #detect collision with tail
+
+    for segment in snake.snakes[1::]:
+        if snake.head.distance(segment) < 10:
+            game_over = True
+            score.end_game()
 
 
 screen.exitonclick()
