f90b0e82a84ae186496751ac630a5f44c8102fb4
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-02-02 04:15:27 +0000

    Update snake.py

--- snake.py
+++ snake.py
@@ -17,11 +17,14 @@ class Snake:
 
     def create_snake(self):
         for snake_index in START_POSITION:
-            new_snake = Turtle(shape="square")
-            new_snake.color("white")
-            new_snake.penup()
-            new_snake.goto(snake_index)
-            self.snakes.append(new_snake)
+            self.add_snake(snake_index)
+
+    def add_snake(self, position):
+        new_snake = Turtle(shape="square")
+        new_snake.color("white")
+        new_snake.penup()
+        new_snake.goto(position)
+        self.snakes.append(new_snake)
 
     def move(self):
         for snake_num in range(len(self.snakes) - 1, 0, -1):
@@ -29,6 +32,9 @@ class Snake:
             new_y = self.snakes[snake_num - 1].ycor()
             self.snakes[snake_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
+
+    def extend(self):
+        self.add_snake(self.snakes[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
