06722199e238d089e7d14e38f9f4760831598689
Jreamz <38327173+Jreamz@users.noreply.github.com>
2021-02-06 23:45:10 +0000

    Update score.py

--- score.py
+++ score.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-
 class Score(Turtle):
 
     def __init__(self):
@@ -10,16 +9,25 @@ class Score(Turtle):
         self.goto(0, 275)
         self.hideturtle()
         self.current_score = 0
+        self.high_score = self.read_score()
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score: {self.current_score}", align="center", font=("Courier", 14, "bold"))
+        self.clear()
+        self.write(f"Score: {self.current_score} High Score: {self.high_score}", align="center", font=("Courier", 14, "bold"))
 
     def add_score(self):
-        self.clear()
         self.current_score += 1
         self.update_score()
 
-    def end_game(self):
-        self.goto(0, 0)
-        self.write(f"G4M3 0V3R", align="center", font=("Courier", 14, "bold"))
+    def reset(self):
+        if self.current_score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.current_score}")
+            self.high_score = self.current_score
+        self.current_score = 0
+        self.update_score()
+
+    def read_score(self):
+        with open("data.txt", mode="r") as file:
+            return int(file.read())
